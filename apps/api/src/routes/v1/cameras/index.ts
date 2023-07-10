e3601764111c3a1e03c60011dc5dfc675e92ab0f
Antoine Kingue <etorefr@gmail.com>
2023-07-10 13:58:06 +0200

    🔒️ update routes security

--- apps/api/src/routes/v1/cameras/index.ts
+++ apps/api/src/routes/v1/cameras/index.ts
@@ -1,11 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
 import CameraModel from "../../../models/Camera";
+import { chechAuth } from "../../../utils/auth";
 
 export const get = async (request: FastifyRequest, _reply: FastifyReply) => {
   const query = request.query as any;
   const limit = query.limit ? parseInt(query.limit as string, 10) : 10;
   const cursor = query.cursor ? parseInt(query.limit as string, 10) : 0;
+
+  const jwtUser = await chechAuth(request);
+
+  if (!jwtUser) {
+    throw new Error("unauthorized");
+  }
 
   const cameras = await CameraModel.find()
     .sort({ createdAt: -1 })
