acd63cab18514fe99a73ea6f5513202812782aed
Antoine Kingue <etorefr@gmail.com>
2023-07-10 05:49:07 +0200

    🎨 improve camera model according designs in api

--- apps/api/src/models/Camera.ts
+++ apps/api/src/models/Camera.ts
@@ -1,10 +1,12 @@
 import { Schema, model } from "mongoose";
 
-const cameraSchema = new Schema({
+import { Camera } from "../types";
+
+const cameraSchema = new Schema<Camera>({
   name: { type: String, required: true },
-  slug: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
 });
 
-const Camera = model("Camera", cameraSchema);
+const CameraModel = model("Camera", cameraSchema);
 
-export default Camera;
+export default CameraModel;
