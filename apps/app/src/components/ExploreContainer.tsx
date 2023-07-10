d1590c17968460d53aeca379226565df7b43158d
Antoine Kingue <etorefr@gmail.com>
2023-07-10 13:55:55 +0200

    🚨 fix lint in explore component in app

--- apps/app/src/components/ExploreContainer.tsx
+++ apps/app/src/components/ExploreContainer.tsx
@@ -1,4 +1,4 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
 interface ContainerProps {
   name: string;
@@ -6,9 +6,18 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
+    <div className={"container"}>
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        {"Explore "}
+        <a
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+          href={"https://ionicframework.com/docs/components"}
+        >
+          {"UI Components"}
+        </a>
+      </p>
     </div>
   );
 };
